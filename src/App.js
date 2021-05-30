99c20a71002df48c1cc483ca4a024f29f4713984
yeri <webothknow@naver.com>
2021-05-30 14:15:50 +0900

    자식개수에 따라 가로사이즈 변경

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Line } from "react-chartjs-2";
 
 function App() {
+  const data = {
+    labels: ["1", "2", "3", "4", "5", "6"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="flex-container">
+        <div class="flex-item">
+          <div class="chart_wrap">
+            1
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="chart_wrap">
+            2
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="chart_wrap">
+            3
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="chart_wrap">
+            4
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="chart_wrap">
+            5
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+        <div class="flex-item">
+          <div class="chart_wrap">
+            6
+            <Line data={data} options={options} />
+            <Line data={data} options={options} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
